18e7c8eede8be4c12382b4094ed18289ed21a097
cmodii <100476911+cmodii@users.noreply.github.com>
2025-08-26 11:13:14 +0100

    Derive implementation

--- src/args.rs
+++ src/args.rs
@@ -1,68 +1,33 @@
-use clap::{command, Arg, Command};
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
-const PNG_PATH_DESC: &str = "A valid file path to a .png file";
-const CHUNK_TYPE_DESC: &str = "A 4 letter string that determines the characteristics of the chunk";
+#[derive(Parser)]
+pub struct Args {
+    #[command(subcommand)]
+    pub cmd: Commands,
+}
 
-pub fn init_cmd() -> Command {
-    command!()
-        .about("(en/de)code hidden messages inside PNG files")
-        .subcommand(Command::new("encode")
-            .about("Encode a new chunk container with a specified message")
-            .arg(
-                Arg::new("File path")
-                    .aliases(["PNG path", "Image path"])
-                    .help(String::from(PNG_PATH_DESC))
-                    .required(true)
-                )
-            .arg(
-                Arg::new("Chunk type")
-                    .help(String::from(CHUNK_TYPE_DESC))
-                    .required(true)
-            )
-            .arg(
-                Arg::new("Message").required(true)
-            )
-            .arg(
-                Arg::new("Output file")
-                    .short('o')
-                    .required(false)
-            )
-        )
-        .subcommand(Command::new("decode")
-            .about("Decode a hidden message inside an existing chunk")
-            .arg(
-                Arg::new("File path")
-                    .aliases(["PNG path", "Image path"])
-                    .help(String::from(PNG_PATH_DESC))
-                    .required(true)
-                )
-            .arg(
-                Arg::new("Chunk type")
-                    .help(String::from(CHUNK_TYPE_DESC))
-                    .required(true)
-            )
-        )
-        .subcommand(Command::new("remove")
-            .about("Remove a chunk (container) for a hidden message")
-            .arg(
-                Arg::new("File path")
-                    .aliases(["PNG path", "Image path"])
-                    .help(String::from(PNG_PATH_DESC))
-                    .required(true)
-                )
-            .arg(
-                Arg::new("Chunk type")
-                    .help(String::from(CHUNK_TYPE_DESC))
-                    .required(true)
-            )
-        )
-        .subcommand(Command::new("print")
-            .about("Print all messages contained in a PNG file")
-            .arg(
-                Arg::new("File path")
-                    .aliases(["PNG path", "Image path"])
-                    .help(String::from(PNG_PATH_DESC))
-                    .required(true)
-                )
-    )
+#[derive(Subcommand, Debug, Clone)]
+pub enum Commands {
+    /// Encode a new chunk container with a specified message
+    Encode {
+        filepath: PathBuf,
+        chunk_code: String,
+        message: String,
+    },
+
+    /// Decode a hidden message inside an existing chunk
+    Decode {
+        filepath: PathBuf,
+        chunk_code: String,
+    },
+
+    /// Remove a chunk (container) for a hidden message
+    Remove {
+        filepath: PathBuf,
+        chunk_code: String,
+    },
+
+    /// Print all messages contained in a PNG file
+    Print {filepath: PathBuf}
 }
